6ba0cd6dcee264a71593b5b64b1df65ae0c35952
Mykhailo <pmtsmichael@gmail.com>
2021-04-05 21:07:15 +0300

    format code of loops.java class

--- complexTask/src/com/lnu/home/Loops.java
+++ complexTask/src/com/lnu/home/Loops.java
@@ -6,47 +6,47 @@ public class Loops {
 
     private double e = Math.E;
 
-    private double getFunction(double x){
-        return x*x-Math.pow(e,x)-2*x;
+    private double getFunction(double x) {
+        return x * x - Math.pow(e, x) - 2 * x;
     }
 
-    private ArrayList<Double> getFunctionInSteps(int a,int b,double h){
+    private ArrayList<Double> getFunctionInSteps(int a, int b, double h) {
         double value = a;
         ArrayList<Double> y = new ArrayList<>();
 
-        while(value<=b){
+        while (value <= b) {
             double yValue = getFunction(value);
             y.add(yValue);
-            value+=h;
+            value += h;
         }
 
         return y;
     }
 
-    private double getMaxY(ArrayList<Double> y){
+    private double getMaxY(ArrayList<Double> y) {
         double maxValue = y.get(0);
 
-        for(int i=1;i<y.size();i++){
-            if(maxValue<y.get(i)){
-                maxValue=y.get(i);
+        for (int i = 1; i < y.size(); i++) {
+            if (maxValue < y.get(i)) {
+                maxValue = y.get(i);
             }
         }
 
         return maxValue;
     }
 
-    public void functionTabulation(int a,int b,double h){
+    public void functionTabulation(int a, int b, double h) {
         double value = a;
-        ArrayList<Double> y = getFunctionInSteps(a,b,h);
+        ArrayList<Double> y = getFunctionInSteps(a, b, h);
 
         System.out.println("Tabulation: \n x \t y");
-        for(int i=0;i<y.size();i++){
+        for (int i = 0; i < y.size(); i++) {
 
             System.out.println(value + "\t" + y.get(i));
-            value+=h;
+            value += h;
         }
 
         double maxValue = getMaxY(y);
-        System.out.println("\nThe biggest value of function is : "+maxValue);
+        System.out.println("\nThe biggest value of function is : " + maxValue);
     }
 }

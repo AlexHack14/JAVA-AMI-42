7b8a40b2502e9a7a8e97786825ffce8b2d57ce48
Mykhailo <pmtsmichael@gmail.com>
2021-04-15 17:57:02 +0300

    formatted ConditionIF class

--- complexTask/src/com/lnu/home/ConditionIF.java
+++ complexTask/src/com/lnu/home/ConditionIF.java
@@ -2,56 +2,51 @@ package com.lnu.home;
 
 public class ConditionIF {
 
-    public void getValueOfX(double x){
+    public void getValueOfX(double x) {
 
-        if(x<=0){
+        if (x <= 0) {
             System.out.println(0);
-        }
-        else if(x>0 && x<=1){
+        } else if (x > 0 && x <= 1) {
             System.out.println(x);
-        }
-        else{
-            System.out.println(x*x);
+        } else {
+            System.out.println(x * x);
         }
     }
 
-    private Boolean isNumberCorrect(int number){
-        if(number>99 && number<1000){
+    private Boolean isNumberCorrect(int number) {
+        if (number > 99 && number < 1000) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    private int getFirstDigit(int number){
+    private int getFirstDigit(int number) {
         int firstDigit = 0;
 
-        while(number!=0){
-            firstDigit=number%10;
-            number/=10;
+        while (number != 0) {
+            firstDigit = number % 10;
+            number /= 10;
         }
         return firstDigit;
     }
 
-    private int getLastDigit(int number){
-        int lastDigit = number%10;
+    private int getLastDigit(int number) {
+        int lastDigit = number % 10;
         return lastDigit;
     }
 
-    public void compareFirstAndLastDigit(int number){
-        if(isNumberCorrect(number)){
+    public void compareFirstAndLastDigit(int number) {
+        if (isNumberCorrect(number)) {
             int firstDigit = getFirstDigit(number);
             int lastDigit = getLastDigit(number);
 
-            if(firstDigit > lastDigit){
-                System.out.println(firstDigit + " bigger than "+lastDigit+" so first digit of a "+number+" is bigger");
+            if (firstDigit > lastDigit) {
+                System.out.println(firstDigit + " bigger than " + lastDigit + " so first digit of a " + number + " is bigger");
+            } else {
+                System.out.println(firstDigit + " smaller than " + lastDigit + " so last digit of a " + number + " is bigger");
             }
-            else{
-                System.out.println(firstDigit + " smaller than "+lastDigit+" so last digit of a "+number+" is bigger");
-            }
-        }
-        else{
+        } else {
             System.out.println("Number is not correct");
         }
     }
